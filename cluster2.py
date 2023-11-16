31636c03c23d542d7d7d3efd37ddffc3e2b98d7d
Antoine Klein <antoineklein2000@gmail.com>
2023-11-16 10:47:26 +0100

    init rapport

--- cluster2.py
+++ cluster2.py
@@ -22,7 +22,7 @@ def distance(a, b):
 # lab_data = np.apply_along_axis(rgb_to_lab, 1, data)
 lab_data = np.apply_along_axis(colour.XYZ_to_Lab, 1, data)
 kmeans.eucledian_distances = distance
-kmeans.fit(lab_data)
+kmeans.fit(data)
 # Get cluster labels and cluster centers
 cluster_labels = kmeans.labels_
 cluster_centers = kmeans.cluster_centers_
@@ -33,9 +33,11 @@ ax = fig.add_subplot(111, projection='3d')
 # Plot points with their assigned clusters
 for i in range(num_clusters):
     # show color of each center
-    if 1==0:
+    if 1==1:
         rgb = cluster_centers[i]
-        print(colored_background(int(rgb[0]), int(rgb[1]), int(rgb[2]), f'Cluster {i + 1}'))
+        len = data[cluster_labels == i].shape[0]
+
+        print(colored_background(int(rgb[0]), int(rgb[1]), int(rgb[2]), f'Cluster {i + 1} : {len}'))
     else:
         lab = cluster_centers[i]
         rgb = colour.Lab_to_XYZ([[lab[0], lab[1], lab[2]]])
@@ -43,17 +45,15 @@ for i in range(num_clusters):
         # show color rgb
         print(colored_background(int(rgb[0][0]), int(rgb[0][1]), int(rgb[0][2]), f'Cluster {i + 1} : {len}'))
 
+# for i in range(num_clusters):
+#     rgb = [data[cluster_labels == i][0][0]/255, data[cluster_labels == i][0][1]/255, data[cluster_labels == i][0][2]/255]
+#     ax.scatter(data[cluster_labels == i, 0], data[cluster_labels == i, 1], data[cluster_labels == i, 2], label=f'Cluster {i + 1}', c=[rgb])
+
 for i in range(num_clusters):
     rgb = [data[cluster_labels == i][0][0]/255, data[cluster_labels == i][0][1]/255, data[cluster_labels == i][0][2]/255]
     ax.scatter(data[cluster_labels == i, 0], data[cluster_labels == i, 1], data[cluster_labels == i, 2], label=f'Cluster {i + 1}', c=[rgb])
 
-# for c in data:
-#     rgb = [c[0]/255, c[1]/255, c[2]/255]
-#     ax.scatter(c[0], c[1], c[2], label=f'Cluster {i + 1}', c=[rgb])
-# Plot cluster centers
-# for i in range(num_clusters):
-#     center_in_lab = colour.XYZ_to_Lab([[cluster_centers[i, 0], cluster_centers[i, 1], cluster_centers[i, 2]]])
-#     ax.scatter(center_in_lab[0][0]*255, center_in_lab[0][1]*255, center_in_lab[0][2]*255, s=100, c='black', marker='x')
+
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
