440498e295cb4931823e5752f3bea6ae26d0a609
Antoine Klein <antoineklein2000@gmail.com>
2023-12-07 09:09:57 +0100

    add comments

--- cluster.py
+++ cluster.py
@@ -35,8 +35,10 @@ def get_data_from_input(path):
 
 def normalize_to_srgb(rgb):
     return np.dot(1/255,rgb)
-dict_skin = get_data_from_input('women-improved.csv')
-# dict_skin = get_data_from_input('input.csv')
+# on construit un dicitionnaire de données de peau et de vêtements associés
+# dict_skin = {'124, 123, 123': {'upper': [255, 255, 255], 'lower': [255, 255, 255], 'skin': [124, 123, 123], 'path': 'path/to/image'},...}
+dict_skin = get_data_from_input('input.csv')
+# dict_skin = get_data_from_input('input.csv') 
 
 skin_data = []
 for data in dict_skin.items():
@@ -65,29 +67,24 @@ kmeans.fit(lab_data)
 cluster_labels = kmeans.labels_
 cluster_centers = kmeans.cluster_centers_
 
-for i in range(num_clusters):
-    lab = cluster_centers[i]
-    rgb = colour.Lab_to_XYZ([[lab[0], lab[1], lab[2]]])
+
+for i in range(num_clusters): # on parcourt les clusters de peau
+    lab = cluster_centers[i] # on récupère le centre du cluster
+    rgb = colour.Lab_to_XYZ([[lab[0], lab[1], lab[2]]]) # on convertit en RGB
     rgb = colour.XYZ_to_RGB(rgb, RGB_COLOURSPACE_sRGB)
     rgb = np.dot(255,rgb)
-    len = skin_data[cluster_labels == i].shape[0]
+    len = skin_data[cluster_labels == i].shape[0] # on récupère le nombre de points dans le cluster
     # show color rgb
     print(colored_background(int(rgb[0][0]), int(rgb[0][1]), int(rgb[0][2]), f'Cluster {i + 1} --------------------------------------------------------------------------------------------------------- {len}'))
-    #get every point in cluster
-    cluster = skin_data[cluster_labels == i]
+    cluster = skin_data[cluster_labels == i] # on récupère les points du cluster `i`
     upper_data = []
     lower_data = []
     for c in cluster:
-        upper_data.append(dict_skin[str(c)]['upper'])
+        upper_data.append(dict_skin[str(c)]['upper']) # récupère les couleurs des vêtements associés dans le dictionnaire
         lower_data.append(dict_skin[str(c)]['lower'])
     upper_data = np.array(upper_data)
     lower_data = np.array(lower_data)
-    # tmp_upper_data = upper_data
-    # tmp_lower_data = lower_data
-    # for j in range(len(upper_data)):
-    #     tmp_upper_data[j] = np.dot(m_lab, tmp_upper_data[j])
-    # for j in range(len(lower_data)):
-    #     tmp_lower_data[j] = np.dot(m_lab, tmp_lower_data[j])
+    # créations de clusters pour les données récupérées (vêtements)
     upper_lab_data = np.apply_along_axis(normalize_to_srgb, 1 , upper_data)
     lower_lab_data = np.apply_along_axis(normalize_to_srgb, 1, lower_data)
     upper_lab_data = np.apply_along_axis(colour.RGB_to_XYZ, 1, upper_lab_data, *color_space)
@@ -106,14 +103,14 @@ for i in range(num_clusters):
     # get the top 3 color
     upper_color = []
     lower_color = []
-    # sort by len of cluster shape
+    # on trie les clusters par nombre de points dans le cluster
     for j in range(n_colors):
         upper_color.append([upper_cluster_centers[j], upper_data[upper_cluster_labels == j].shape[0]])
         lower_color.append([lower_cluster_centers[j], lower_data[lower_cluster_labels == j].shape[0]])
     upper_color = sorted(upper_color, key=lambda x: x[1], reverse=True)
     lower_color = sorted(lower_color, key=lambda x: x[1], reverse=True)
 
-    # print top 3 color
+    # on affiche les 5 premiers clusters pour les vêtements
     for c in upper_color[:5]:
         lab = c[0]
         # lab = np.dot(mm_lab, lab)
@@ -133,10 +130,6 @@ for i in range(num_clusters):
         if diff >15:
             print(colored_background(int(rgb[0][0]), int(rgb[0][1]), int(rgb[0][2]), f'Lower color ------------------------------ {c[1]} | {lab} | {diff}'))
 
-    cross_table = []
-    for u in range(n_colors):
-        for l in range(n_colors):
-            cross_table[u][j] = 0
 
     
 
