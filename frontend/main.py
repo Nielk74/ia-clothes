dbbd15e57dcae0842594b1cf9a3d5486ae0c0f8c
Monica <monica.huynh@grenoble-inp.org>
2024-01-16 21:14:17 +0100

    Remove width on images for responsive + full size on fullscreen size

--- frontend/main.py
+++ frontend/main.py
@@ -156,9 +156,9 @@ def get_result(file_name, dataset, gender, cluster_size):
     col3.markdown(f"Lower color: {write_color(rgb_lower)}", unsafe_allow_html=True)
     col4.write("Example image:")
     if (dataset == "DeepFashion-MultiModal"):
-      col4.image(requests.get(f'https://raw.githubusercontent.com/Nielk74/ia-clothes/master/data/results/deepfashion/example-images/{value["path"]}.jpg').content, width=100)
+      col4.image(requests.get(f'https://raw.githubusercontent.com/Nielk74/ia-clothes/master/data/results/deepfashion/example-images/{value["path"]}.jpg').content)
     else:
-      col4.image(requests.get(f'https://raw.githubusercontent.com/Nielk74/ia-clothes/master/data/results/style-du-monde/example-images/{value["path"].split("/")[-1]}').content, width=100)
+      col4.image(requests.get(f'https://raw.githubusercontent.com/Nielk74/ia-clothes/master/data/results/style-du-monde/example-images/{value["path"].split("/")[-1]}').content)
     # write path in a file
     # f = open(f"images-path-{dataset}", "a")
     # f = open(f"images-path-{gender}-{cluster_size}", "a")
